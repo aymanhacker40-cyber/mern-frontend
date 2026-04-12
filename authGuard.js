1d20a018dbb8ad6569d266593141e53e94cc418d
aymanhacker40-cyber <aymanhacker40@gmail.com>
2026-04-12 20:38:47 +0200

    news

--- authGuard.js
+++ authGuard.js
@@ -1,7 +1,27 @@
-// check token from local OR session storage
-const token = localStorage.getItem("token") || sessionStorage.getItem("token");
+(function () {
+  const token = localStorage.getItem("token");
 
-if (!token) {
-  window.location.href =
-    window.location.origin + "/registration/login/travel_login_html.html";
-}
+  // الصفحات المسموح تدخلها من غير login
+  const publicPages = [
+    "/registration/login/travel_login_html.html",
+    "/registration/signup.html",
+    "/registration/login/reset-password.html",
+    "/registration/login/travel_forgot_password.html"
+  ];
+
+  const currentPage = window.location.pathname;
+
+  const isPublic = publicPages.some(page =>
+    currentPage.includes(page)
+  );
+
+  // ❌ لو مفيش توكن ومش صفحة عامة → رجعه للوجين
+  if (!token && !isPublic) {
+    window.location.href = "/registration/login/travel_login_html.html";
+  }
+  // لو عامل login وفتح login page → رجعه للهوم
+  if (token && currentPage.includes("travel_login_html.html")) {
+    window.location.href = "/home/index.html";
+  }
+
+})();
